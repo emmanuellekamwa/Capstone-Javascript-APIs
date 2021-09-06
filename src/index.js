e9450ca6ed9048c9659c4f2ba5d9d9f3e09cf99a
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-09-06 22:12:34 +0200

    Create reservationsPopup function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 /* eslint no-unused-vars:0 */
 import _ from 'lodash';
 import './style.css';
+import reservationsPopup from './reservationsPopup';
+
+const object = {
+symbol:	"BTCUSDT",
+priceChange:	"1474.87000000",
+priceChangePercent:	"2.928",
+weightedAvgPrice:	"51565.37326695",
+prevClosePrice:	"50379.46000000",
+lastPrice:	"51854.33000000",
+lastQty:	"0.00552000",
+bidPrice:	"51854.32000000",
+bidQty:	"1.34138000",
+askPrice:	"51854.33000000",
+askQty:	"1.83474000",
+openPrice:	"50379.46000000",
+highPrice:	"52188.80000000",
+lowPrice:	"50355.00000000",
+volume:	"52808.54617200",
+quoteVolume:	"2723092395.04434399",
+openTime:	1630868367651,
+closeTime:	1630954767651,
+firstId:	1039031513,
+lastId:	1040812366,
+count:	1780854,
+}
+
+reservationsPopup(object);
---
+++ src/reservationsPopup.js
@@ -0,0 +1,34 @@
+
+const reservationsPopup =  (object) => {
+
+  const popupWindow = document.getElementById('popup');
+  popupWindow.className = "popup";
+  const reservation = document.createElement('div');
+  reservation.className = 'reservation';
+  const img = document.createElement('img');
+  const name = document.createElement('h3');
+  img.setAttribute('src', `img/${object.symbol}.png`);
+  name.innerText = object.symbol;
+  const stats = document.createElement('div');
+  stats.className = 'stats';
+  const lastPrice = document.createElement('p');
+  const openPrice = document.createElement('p');
+  const highPrice = document.createElement('p');
+  const lowPrice = document.createElement('p');
+  lastPrice.innerText = `Last price: ${object.lastPrice}`;
+  openPrice.innerText = `Open price: ${object.openPrice}`;
+  highPrice.innerText = `High price: ${object.highPrice}`;
+  lowPrice.innerText = `Low price: ${object.lastPrice}`;
+  stats.innerHTML = lastPrice.outerHTML 
+  + openPrice.outerHTML
+  + highPrice.outerHTML
+  + lowPrice.outerHTML;
+  reservation.innerHTML = img.outerHTML
+  + name.outerHTML
+  + stats.outerHTML;  
+  popupWindow.appendChild(reservation);
+  
+
+}
+
+export default reservationsPopup;
