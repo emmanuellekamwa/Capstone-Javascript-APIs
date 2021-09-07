2258887f5b997bea566fdf3a3ca0f9e811c81dbc
Reshy <reshy@protonmail.com>
2021-09-07 10:01:26 +0200

    Fix linter errors

--- src/getData.js
+++ src/getData.js
@@ -1,6 +1,5 @@
 export default async () => {
-    const request = await fetch('https://api.binance.com/api/v1/ticker/24hr');
-    const result = await request.json();
-    return result;
-  };
-  
+  const request = await fetch('https://api.binance.com/api/v1/ticker/24hr');
+  const result = await request.json();
+  return result;
+};
