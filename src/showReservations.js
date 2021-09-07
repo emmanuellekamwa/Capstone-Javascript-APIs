e5056f3ca20b8abe2aeff875531e59e88b0079c7
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-09-07 16:33:42 +0200

    Fix linters

--- src/showReservations.js
+++ src/showReservations.js
@@ -5,10 +5,10 @@ const showReservations = async (reserveCont, id) => {
   const heading = document.createElement('h4');
   const list = document.createElement('ul');
   if (reservations) {
-    reservations.forEach( reservation => {
+    reservations.forEach((reservation) => {
       list.innerHTML += `<li>${reservation.date_start} - ${reservation.date_end} by ${reservation.username}</li>`;
     });
-  };
+  }
   reserveCont.innerHTML += heading.outerHTML
   + list.outerHTML;
 };
