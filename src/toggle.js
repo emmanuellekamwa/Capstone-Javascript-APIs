217eabce09d48f665b3cdf653fe65b3bc3b6c1af
Reshy <reshy@protonmail.com>
2021-09-08 17:17:36 +0200

    Add styling for popup

--- src/toggle.js
+++ src/toggle.js
@@ -1,6 +1,14 @@
 const displayToggle = (element) => {
   // eslint-disable-next-line no-unused-expressions
   element.style.display === 'none' ? element.style.display = 'block' : element.style.display = 'none';
+  const title = document.getElementById('main-nav');
+  const nav = document.getElementById('main-title');
+  const section = document.getElementById('main-section');
+  const footer = document.getElementById('footer');
+  title.classList.toggle('blurred');
+  section.classList.toggle('blurred');
+  nav.classList.toggle('blurred');
+  footer.classList.toggle('blurred');
 };
 
 export default displayToggle;
