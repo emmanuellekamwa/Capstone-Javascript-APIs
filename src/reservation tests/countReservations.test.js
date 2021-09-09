8929f71a8c5bb02814b19d24609d2d8e1dec5773
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-09-09 10:26:46 +0200

    Create tests for reservation counter function

---
+++ src/reservation tests/countReservations.test.js
@@ -0,0 +1,28 @@
+import countReservations from '../countReservations';
+
+describe('Reservations count', () => {
+  it('retuns number of reservations', () => {
+    const reservations = [
+      {
+        name: 'John',
+        date_start: '2021/12/12',
+        date_end: '2021/12/13',
+      },
+      {
+        name: 'Emma',
+        date_start: '2021/12/11',
+        date_end: '2021/12/13',
+      },
+      {
+        name: 'Stewart',
+        date_start: '2021/12/08',
+        date_end: '2021/12/09',
+      },
+    ];
+    expect(countReservations(reservations)).toEqual(3);
+  });
+  it('returns 0 if there are no reservations', () => {
+    const reservations = [];
+    expect(countReservations(reservations)).toEqual(0);
+  });
+})
--- src/showReservations.js
+++ src/showReservations.js
@@ -3,7 +3,7 @@ import getReservations from './getReservations';
 const showReservations = async (reserveCont, id) => {
   const reservations = await getReservations(id);
   const heading = document.createElement('h4');
-  heading.innerHTML = `Reservations <span id='counter'>${reservations.length || 0}</span>`;
+  heading.innerHTML = `Reservations <span id='counter'>(${countReservations(reservations)})</span>`;
   const list = document.createElement('ul');
   list.id = 'reservationList';
   if (Array.isArray(reservations)) {
